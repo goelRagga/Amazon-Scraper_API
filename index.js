24b641b73e6df8e8e21f0ae050b75006bded43b7
goelRagga <raghavgoel847@gmail.com>
2021-09-22 11:31:47 +0530

    Settitng up server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express =require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Amazon Scraper API by Raghav.");
+})
+
+
+app.listen(PORT, () => console.log(`Server Running on Port : ${PORT}`) );
